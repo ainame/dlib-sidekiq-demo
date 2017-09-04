e7b19048fa43e486390de1b2d59a74028b427c39
ainame <s.namai.09@gmail.com>
2017-09-05 03:47:08 +0900

    Simplify

--- app/jobs/face_detection_job.rb
+++ app/jobs/face_detection_job.rb
@@ -3,12 +3,11 @@ class FaceDetectionJob
 
   def perform(image_id)
     image = Image.find_by(id: image_id)
-    return unless image
-    return if image.faces.exists?
+    return if image.blank? || image.faces.exists?
 
     detector = Dlib::DNNFaceDetector.new(model_path)
     frames = image.download { |file| detector.detect(Dlib::Image.load(file.path)) }
-    frames.map { |f| Face.create!(image_id: image.id, x: f.left, y: f.top, width: f.width, height: f.height) }
+    frames.each { |f| Face.create!(image_id: image.id, x: f.left, y: f.top, width: f.width, height: f.height) }
   end
 
   def model_path
